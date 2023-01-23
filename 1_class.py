3b60a2438f3aa23e6739a04344972c0dacd936c0
sardaruzair1 <sardarsafeer012012@gmail.com>
2023-01-23 20:03:42 +0500

    oop in python 01

--- 1_class.py
+++ 1_class.py
@@ -1,2 +1,21 @@
 class Employee:
     name = "sardar"
+    role = "Software Developer"
+    salry = 20
+    def info(self):
+        print(f"{self.name} is a {self.role}")
+
+a = Employee()
+a.name = "uzair"
+a.role = "Data Scientest"
+a.salry = 50
+
+
+b = Employee()
+b.name = "usman"
+b.role = "HR"
+b.salry = 40
+
+
+print(a.info())
+print(b.info())
